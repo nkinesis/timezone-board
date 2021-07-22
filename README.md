6dc27586cee05238aec6a3db3ff565fcde4b39dd
Gabriel Ullmann <gabriel.ullmann@agenciagh.com.br>
2021-07-22 11:30:56 -0300

    First

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# timezone-board
+# Timezone board
+By Gabriel C. Ullmann, 2021
+
+## About
 Timezone board to help me follow contacts around the world
+
+## How to use?
+Open timezones.html on browser and inform the link to a JSON file with the structure below. Timezones names must follow IANA standards: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+
+    [
+        {
+            "name": "Jane Doe"
+            "company": "Example.inc"
+            "country": "England"
+            "timezone": "Europe/London"
+        },
+        {
+            "name": "Gabriel Ullmann"
+            "company": "Empresa Ltda."
+            "country": "Brazil"
+            "timezone": "America/Sao_Paulo"
+        }
+    ]
---
+++ main.js
@@ -0,0 +1,501 @@
+//Test: https://sheetdb.io/api/v1/u5f8mci3ox1ha
+
+const TIMEZONES = [
+    'Europe/Andorra',
+    'Asia/Dubai',
+    'Asia/Kabul',
+    'Europe/Tirane',
+    'Asia/Yerevan',
+    'Antarctica/Casey',
+    'Antarctica/Davis',
+    'Antarctica/DumontDUrville',
+    'Antarctica/Mawson',
+    'Antarctica/Palmer',
+    'Antarctica/Rothera',
+    'Antarctica/Syowa',
+    'Antarctica/Troll',
+    'Antarctica/Vostok',
+    'America/Argentina/Buenos_Aires',
+    'America/Argentina/Cordoba',
+    'America/Argentina/Salta',
+    'America/Argentina/Jujuy',
+    'America/Argentina/Tucuman',
+    'America/Argentina/Catamarca',
+    'America/Argentina/La_Rioja',
+    'America/Argentina/San_Juan',
+    'America/Argentina/Mendoza',
+    'America/Argentina/San_Luis',
+    'America/Argentina/Rio_Gallegos',
+    'America/Argentina/Ushuaia',
+    'Pacific/Pago_Pago',
+    'Europe/Vienna',
+    'Australia/Lord_Howe',
+    'Antarctica/Macquarie',
+    'Australia/Hobart',
+    'Australia/Currie',
+    'Australia/Melbourne',
+    'Australia/Sydney',
+    'Australia/Broken_Hill',
+    'Australia/Brisbane',
+    'Australia/Lindeman',
+    'Australia/Adelaide',
+    'Australia/Darwin',
+    'Australia/Perth',
+    'Australia/Eucla',
+    'Asia/Baku',
+    'America/Barbados',
+    'Asia/Dhaka',
+    'Europe/Brussels',
+    'Europe/Sofia',
+    'Atlantic/Bermuda',
+    'Asia/Brunei',
+    'America/La_Paz',
+    'America/Noronha',
+    'America/Belem',
+    'America/Fortaleza',
+    'America/Recife',
+    'America/Araguaina',
+    'America/Maceio',
+    'America/Bahia',
+    'America/Sao_Paulo',
+    'America/Campo_Grande',
+    'America/Cuiaba',
+    'America/Santarem',
+    'America/Porto_Velho',
+    'America/Boa_Vista',
+    'America/Manaus',
+    'America/Eirunepe',
+    'America/Rio_Branco',
+    'America/Nassau',
+    'Asia/Thimphu',
+    'Europe/Minsk',
+    'America/Belize',
+    'America/St_Johns',
+    'America/Halifax',
+    'America/Glace_Bay',
+    'America/Moncton',
+    'America/Goose_Bay',
+    'America/Blanc-Sablon',
+    'America/Toronto',
+    'America/Nipigon',
+    'America/Thunder_Bay',
+    'America/Iqaluit',
+    'America/Pangnirtung',
+    'America/Atikokan',
+    'America/Winnipeg',
+    'America/Rainy_River',
+    'America/Resolute',
+    'America/Rankin_Inlet',
+    'America/Regina',
+    'America/Swift_Current',
+    'America/Edmonton',
+    'America/Cambridge_Bay',
+    'America/Yellowknife',
+    'America/Inuvik',
+    'America/Creston',
+    'America/Dawson_Creek',
+    'America/Fort_Nelson',
+    'America/Vancouver',
+    'America/Whitehorse',
+    'America/Dawson',
+    'Indian/Cocos',
+    'Europe/Zurich',
+    'Africa/Abidjan',
+    'Pacific/Rarotonga',
+    'America/Santiago',
+    'America/Punta_Arenas',
+    'Pacific/Easter',
+    'Asia/Shanghai',
+    'Asia/Urumqi',
+    'America/Bogota',
+    'America/Costa_Rica',
+    'America/Havana',
+    'Atlantic/Cape_Verde',
+    'America/Curacao',
+    'Indian/Christmas',
+    'Asia/Nicosia',
+    'Asia/Famagusta',
+    'Europe/Prague',
+    'Europe/Berlin',
+    'Europe/Copenhagen',
+    'America/Santo_Domingo',
+    'Africa/Algiers',
+    'America/Guayaquil',
+    'Pacific/Galapagos',
+    'Europe/Tallinn',
+    'Africa/Cairo',
+    'Africa/El_Aaiun',
+    'Europe/Madrid',
+    'Africa/Ceuta',
+    'Atlantic/Canary',
+    'Europe/Helsinki',
+    'Pacific/Fiji',
+    'Atlantic/Stanley',
+    'Pacific/Chuuk',
+    'Pacific/Pohnpei',
+    'Pacific/Kosrae',
+    'Atlantic/Faroe',
+    'Europe/Paris',
+    'Europe/London',
+    'Asia/Tbilisi',
+    'America/Cayenne',
+    'Africa/Accra',
+    'Europe/Gibraltar',
+    'America/Godthab',
+    'America/Danmarkshavn',
+    'America/Scoresbysund',
+    'America/Thule',
+    'Europe/Athens',
+    'Atlantic/South_Georgia',
+    'America/Guatemala',
+    'Pacific/Guam',
+    'Africa/Bissau',
+    'America/Guyana',
+    'Asia/Hong_Kong',
+    'America/Tegucigalpa',
+    'America/Port-au-Prince',
+    'Europe/Budapest',
+    'Asia/Jakarta',
+    'Asia/Pontianak',
+    'Asia/Makassar',
+    'Asia/Jayapura',
+    'Europe/Dublin',
+    'Asia/Jerusalem',
+    'Asia/Kolkata',
+    'Indian/Chagos',
+    'Asia/Baghdad',
+    'Asia/Tehran',
+    'Atlantic/Reykjavik',
+    'Europe/Rome',
+    'America/Jamaica',
+    'Asia/Amman',
+    'Asia/Tokyo',
+    'Africa/Nairobi',
+    'Asia/Bishkek',
+    'Pacific/Tarawa',
+    'Pacific/Enderbury',
+    'Pacific/Kiritimati',
+    'Asia/Pyongyang',
+    'Asia/Seoul',
+    'Asia/Almaty',
+    'Asia/Qyzylorda',
+    'Asia/Qostanay',
+    'Asia/Aqtobe',
+    'Asia/Aqtau',
+    'Asia/Atyrau',
+    'Asia/Oral',
+    'Asia/Beirut',
+    'Asia/Colombo',
+    'Africa/Monrovia',
+    'Europe/Vilnius',
+    'Europe/Luxembourg',
+    'Europe/Riga',
+    'Africa/Tripoli',
+    'Africa/Casablanca',
+    'Europe/Monaco',
+    'Europe/Chisinau',
+    'Pacific/Majuro',
+    'Pacific/Kwajalein',
+    'Asia/Yangon',
+    'Asia/Ulaanbaatar',
+    'Asia/Hovd',
+    'Asia/Choibalsan',
+    'Asia/Macau',
+    'America/Martinique',
+    'Europe/Malta',
+    'Indian/Mauritius',
+    'Indian/Maldives',
+    'America/Mexico_City',
+    'America/Cancun',
+    'America/Merida',
+    'America/Monterrey',
+    'America/Matamoros',
+    'America/Mazatlan',
+    'America/Chihuahua',
+    'America/Ojinaga',
+    'America/Hermosillo',
+    'America/Tijuana',
+    'America/Bahia_Banderas',
+    'Asia/Kuala_Lumpur',
+    'Asia/Kuching',
+    'Africa/Maputo',
+    'Africa/Windhoek',
+    'Pacific/Noumea',
+    'Pacific/Norfolk',
+    'Africa/Lagos',
+    'America/Managua',
+    'Europe/Amsterdam',
+    'Europe/Oslo',
+    'Asia/Kathmandu',
+    'Pacific/Nauru',
+    'Pacific/Niue',
+    'Pacific/Auckland',
+    'Pacific/Chatham',
+    'America/Panama',
+    'America/Lima',
+    'Pacific/Tahiti',
+    'Pacific/Marquesas',
+    'Pacific/Gambier',
+    'Pacific/Port_Moresby',
+    'Pacific/Bougainville',
+    'Asia/Manila',
+    'Asia/Karachi',
+    'Europe/Warsaw',
+    'America/Miquelon',
+    'Pacific/Pitcairn',
+    'America/Puerto_Rico',
+    'Asia/Gaza',
+    'Asia/Hebron',
+    'Europe/Lisbon',
+    'Atlantic/Madeira',
+    'Atlantic/Azores',
+    'Pacific/Palau',
+    'America/Asuncion',
+    'Asia/Qatar',
+    'Indian/Reunion',
+    'Europe/Bucharest',
+    'Europe/Belgrade',
+    'Europe/Kaliningrad',
+    'Europe/Moscow',
+    'Europe/Simferopol',
+    'Europe/Kirov',
+    'Europe/Astrakhan',
+    'Europe/Volgograd',
+    'Europe/Saratov',
+    'Europe/Ulyanovsk',
+    'Europe/Samara',
+    'Asia/Yekaterinburg',
+    'Asia/Omsk',
+    'Asia/Novosibirsk',
+    'Asia/Barnaul',
+    'Asia/Tomsk',
+    'Asia/Novokuznetsk',
+    'Asia/Krasnoyarsk',
+    'Asia/Irkutsk',
+    'Asia/Chita',
+    'Asia/Yakutsk',
+    'Asia/Khandyga',
+    'Asia/Vladivostok',
+    'Asia/Ust-Nera',
+    'Asia/Magadan',
+    'Asia/Sakhalin',
+    'Asia/Srednekolymsk',
+    'Asia/Kamchatka',
+    'Asia/Anadyr',
+    'Asia/Riyadh',
+    'Pacific/Guadalcanal',
+    'Indian/Mahe',
+    'Africa/Khartoum',
+    'Europe/Stockholm',
+    'Asia/Singapore',
+    'America/Paramaribo',
+    'Africa/Juba',
+    'Africa/Sao_Tome',
+    'America/El_Salvador',
+    'Asia/Damascus',
+    'America/Grand_Turk',
+    'Africa/Ndjamena',
+    'Indian/Kerguelen',
+    'Asia/Bangkok',
+    'Asia/Dushanbe',
+    'Pacific/Fakaofo',
+    'Asia/Dili',
+    'Asia/Ashgabat',
+    'Africa/Tunis',
+    'Pacific/Tongatapu',
+    'Europe/Istanbul',
+    'America/Port_of_Spain',
+    'Pacific/Funafuti',
+    'Asia/Taipei',
+    'Europe/Kiev',
+    'Europe/Uzhgorod',
+    'Europe/Zaporozhye',
+    'Pacific/Wake',
+    'America/New_York',
+    'America/Detroit',
+    'America/Kentucky/Louisville',
+    'America/Kentucky/Monticello',
+    'America/Indiana/Indianapolis',
+    'America/Indiana/Vincennes',
+    'America/Indiana/Winamac',
+    'America/Indiana/Marengo',
+    'America/Indiana/Petersburg',
+    'America/Indiana/Vevay',
+    'America/Chicago',
+    'America/Indiana/Tell_City',
+    'America/Indiana/Knox',
+    'America/Menominee',
+    'America/North_Dakota/Center',
+    'America/North_Dakota/New_Salem',
+    'America/North_Dakota/Beulah',
+    'America/Denver',
+    'America/Boise',
+    'America/Phoenix',
+    'America/Los_Angeles',
+    'America/Anchorage',
+    'America/Juneau',
+    'America/Sitka',
+    'America/Metlakatla',
+    'America/Yakutat',
+    'America/Nome',
+    'America/Adak',
+    'Pacific/Honolulu',
+    'America/Montevideo',
+    'Asia/Samarkand',
+    'Asia/Tashkent',
+    'America/Caracas',
+    'Asia/Ho_Chi_Minh',
+    'Pacific/Efate',
+    'Pacific/Wallis',
+    'Pacific/Apia',
+    'Africa/Johannesburg'
+];
+
+var UTILS = {
+    detectLocale: function () {
+        // if the user understands portuguese, prefer portuguese
+        var langs = window.navigator.languages ? window.navigator.languages : [];
+        if (langs.indexOf("pt-BR") > -1) {
+            return "pt-BR";
+        }
+        return window.navigator.language;
+    },
+    isWorkingHours: function (hour) {
+        // 9-to-5 shift +1h (since Brazil is usually 8-to-6)
+        return (hour > 8 && hour < 18);
+    },
+    isWeekend: function (weekDay) {
+        // saturday or sunday
+        return (weekDay == 0 || weekDay == 6);
+    },
+    isValidTimezone: function (timezoneName) {
+        return TIMEZONES.indexOf(timezoneName);
+    },
+}
+
+var APP = {
+    locale: UTILS.detectLocale(),
+    lastUpdateElement: document.querySelector("#lastUpdate"),
+    board: null,
+    people: [],
+    state: 0, //initial loading
+
+    init: function () {
+        APP.updateBoard();
+        setInterval(function () {
+            if (APP.state == 1) {
+                APP.updateBoard();
+            }
+        }, 30000);
+    },
+
+    setLastUpdate() {
+        let dateObj = new Date();
+        let formattedDate = dateObj.toLocaleString(APP.locale);
+        APP.lastUpdateElement.innerText = formattedDate;
+    },
+
+    updateBoard: function () {
+        var jsonUrl = window.localStorage.getItem('jsonURL');
+        if (!jsonUrl) {
+            jsonUrl = prompt("Please inform your JSON file URL:");
+            if (jsonUrl) {
+                window.localStorage.setItem('jsonURL', jsonUrl);
+            } else {
+                APP.state = 2; //error
+                alert("Invalid URL! Please reload the page and try again.");
+            }
+        }
+
+        let requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+        
+        fetch(jsonUrl, requestOptions)
+            .then(response => response.text())
+            .then(function (result) {
+                APP.people = JSON.parse(result);
+                APP.createBoard();
+                APP.setLastUpdate();
+                APP.state = 1; //ok
+            })
+            .catch(function (error) {
+                APP.state = 2; //error
+                console.log(error);
+                alert("Error: " + error);
+            });
+    },
+
+    getPersonTime: function (timezoneName) {
+        //check if timezone exists
+        let tzExists = UTILS.isValidTimezone(timezoneName);
+        if (!tzExists) {
+            return { "fullDate": "Invalid timezone", "workingHours": false, "weekend": true };
+        }
+        let dateObj = new Date();
+        let isoStr = dateObj.toISOString();
+        dateObj = new Date(isoStr);
+        let fullDate = dateObj.toLocaleString(APP.locale, { timeZone: timezoneName });
+        let workingHours = UTILS.isWorkingHours(dateObj.getHours());
+        let weekend = UTILS.isWeekend(dateObj.getDay());
+        return { "fullDate": fullDate, "workingHours": workingHours, "weekend": weekend };
+    },
+
+    createBoard: function () {
+        let counter = 1;
+        APP.board = document.createElement("tbody");
+
+        for (let person of APP.people) {
+            let rowClassName = "";
+
+            // table row and columns
+            let row = document.createElement("tr");
+            let td1 = document.createElement("td");
+            let td2 = document.createElement("td");
+            let td3 = document.createElement("td");
+            let td4 = document.createElement("td");
+            let td5 = document.createElement("td");
+
+            let personTime = APP.getPersonTime(person.timezone);
+
+            td1.innerText = person.name;
+            td2.innerText = person.company;
+            td3.innerText = person.country;
+
+            // show current time and timezone
+            td4.innerText = personTime.fullDate;
+            td4.setAttribute("title", person.timezone);
+
+            // show if person is in working hours
+            td5.innerText = (personTime.workingHours && !personTime.weekend) ? "yes" : "no";
+            if (!personTime.workingHours || personTime.weekend) {
+                rowClassName = " off";
+            } else {
+                rowClassName = "";
+            }
+
+            // assemble table
+            row.appendChild(td1);
+            row.appendChild(td2);
+            row.appendChild(td3);
+            row.appendChild(td4);
+            row.appendChild(td5);
+
+            row.className = "table-light" + rowClassName;
+            if (counter % 2 == 0) {
+                row.className = "table-primary" + rowClassName;
+            }
+
+            APP.board.appendChild(row);
+            counter++;
+        }
+        let curTbody = document.querySelector("tbody");
+        let curTable = curTbody.parentNode;
+        curTable.removeChild(curTbody);
+        curTable.appendChild(APP.board);
+    },
+
+}
+
+APP.init();
